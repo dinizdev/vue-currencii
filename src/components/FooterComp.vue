cc5e1c7cb151ae2c3f47bc87a9ccade5c4563fa1
dinizdev <dinizdevmaster@gmail.com>
2023-12-06 00:35:22 -0300

    fix links footer

--- src/components/FooterComp.vue
+++ src/components/FooterComp.vue
@@ -1,16 +1,24 @@
 <template>
-  <footer class=" grid text-sm bg-green-600 items-center text-white p-2 ">
-    <nav class="grid grid-cols-2 gap-2 w-full max-w-xl mx-auto">
-      <ul class="grid grid-cols-2 gap-1 underline">
-        <li v-for="link in  links " key="id">
-          <a :href="link.link">{{ link.title }}</a>
-        </li>
-      </ul>
-      <p class="flex justify-center place-items-end itens-end text-white font-semibold">All rights reserved 2023 Currencii
+  <div>
+    <!-- Conteúdo da sua página -->
+
+    <footer class="flex flex-col justify-center items-center text-sm bg-green-600  text-white p-2 fixed bottom-0 w-full">
+      <nav class="flex items-center justify-center gap-2 w-full max-w-xl mx-auto">
+        <ul class="flex p-4 grid-cols-2 gap-4 underline">
+          <li v-for="(link, index) in links" :key="index">
+            <a :href="link.link">{{ link.title }}</a>
+          </li>
+        </ul>
+      </nav>
+      <p class="flex w-full bg-gray-900 p-2 justify-center items-end text-white font-semibold">All rights reserved 2023
+        &copy;
+        Currencii
       </p>
-    </nav>
-  </footer>
+    </footer>
+  </div>
 </template>
+
 <script setup>
 import links from "../utils/links"
 </script>
+
